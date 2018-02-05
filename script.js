2cef39cd21c2ce7a3f1f9ecc3aa02a47c103dbc9
Dominick Colandrea <dominickcolandreadev@gmail.com>
2018-02-05 17:15:41 -0500

    Add additional collapsable dropdown funcitonality. WIll scroll up after making selection.

--- script.js
+++ script.js
@@ -79,4 +79,18 @@ $("#contact").click(function(){
 	location.replace("#contact");
 })//end contact click
 
+    $(document).on('click', function(event){
+      var $clickedOn = $(event.target),
+          $collapsableItems = $('.collapse'),
+          isToggleButton = ($clickedOn.closest('.navbar-toggle').length == 1),
+          isLink = ($clickedOn.closest('a').length == 1),
+          isOutsideNavbar = ($clickedOn.parents('.navbar').length == 0);
+
+      if( (!isToggleButton && isLink) || isOutsideNavbar ) {
+        $collapsableItems.each(function(){
+          $(this).collapse('hide');
+        });
+      }
+    });
+
 })//end ready
