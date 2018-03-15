2707c870d2d744090689b658c5d5b497d3789dde
Dominick Colandrea <dominickcolandreadev@gmail.com>
2018-03-15 03:01:25 -0400

    add scroll to top on page change

--- script2.js
+++ script2.js
@@ -5,6 +5,7 @@ $(function() {
 		$(".container").addClass("hidden-xs-up");
 		$(".home").removeClass("hidden-xs-up");
 		location.replace("#home");
+		document.body.scrollTop = document.documentElement.scrollTop = 0;
 	}) //end home click
 	$("#drones").click(function() {
 		$(".link").removeClass("active");
@@ -13,6 +14,7 @@ $(function() {
 		$(".container").addClass("hidden-xs-up");
 		$(".drones").removeClass("hidden-xs-up");
 		location.replace("#drones");
+		document.body.scrollTop = document.documentElement.scrollTop = 0;
 	}) //end drones click
 	$("#pilots").click(function() {
 		$(".link").removeClass("active");
@@ -21,6 +23,7 @@ $(function() {
 		$(".container").addClass("hidden-xs-up");
 		$(".pilots").removeClass("hidden-xs-up");
 		location.replace("#pilots");
+		document.body.scrollTop = document.documentElement.scrollTop = 0;
 	}) //end pilots click
 	$("#licenses").click(function() {
 		$(".link").removeClass("active");
@@ -29,6 +32,7 @@ $(function() {
 		$(".container").addClass("hidden-xs-up");
 		$(".licenses").removeClass("hidden-xs-up");
 		location.replace("#licenses");
+		document.body.scrollTop = document.documentElement.scrollTop = 0;
 	}) //end licenses click
 	$("#eventP").click(function() {
 		$(".link").removeClass("active");
@@ -37,6 +41,7 @@ $(function() {
 		$(".container").addClass("hidden-xs-up");
 		$(".eventP").removeClass("hidden-xs-up");
 		location.replace("#eventPhotos");
+		document.body.scrollTop = document.documentElement.scrollTop = 0;
 	}) //end eventP click
 	$("#realEstateP").click(function() {
 		$(".link").removeClass("active");
@@ -45,6 +50,7 @@ $(function() {
 		$(".container").addClass("hidden-xs-up");
 		$(".realEstateP").removeClass("hidden-xs-up");
 		location.replace("#realEstatePhotos");
+		document.body.scrollTop = document.documentElement.scrollTop = 0;
 	}) //end realEstateP click
 	$("#weddingP").click(function() {
 		$(".link").removeClass("active");
@@ -53,6 +59,7 @@ $(function() {
 		$(".container").addClass("hidden-xs-up");
 		$(".weddingP").removeClass("hidden-xs-up");
 		location.replace("#weddingPhotos");
+		document.body.scrollTop = document.documentElement.scrollTop = 0;
 	}) //end weddingP click
 	$("#aerial").click(function() {
 		$(".link").removeClass("active");
@@ -61,6 +68,7 @@ $(function() {
 		$(".container").addClass("hidden-xs-up");
 		$(".aerial").removeClass("hidden-xs-up");
 		location.replace("#aerialInspections");
+		document.body.scrollTop = document.documentElement.scrollTop = 0;
 	}) //end aerial click
 	$("#contact").click(function() {
 		$(".link").removeClass("active");
@@ -68,6 +76,7 @@ $(function() {
 		$(".container").addClass("hidden-xs-up");
 		$(".contact").removeClass("hidden-xs-up");
 		location.replace("#contact");
+		document.body.scrollTop = document.documentElement.scrollTop = 0;
 	}) //end contact click
 	$(document).on('click', function(event) {
 		var $clickedOn = $(event.target),
